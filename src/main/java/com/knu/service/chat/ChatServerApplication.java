032ba98f23bf29e6bad81e998624f1b539b291ca
Stas Dombrovskyi <727727stas@gmail.com>
2021-12-05 11:35:18 +0200

    Add SpringBoot

--- src/main/java/com/knu/service/chat/ChatServerApplication.java
+++ src/main/java/com/knu/service/chat/ChatServerApplication.java
@@ -1,10 +1,14 @@
 package com.knu.service.chat;
+
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
 import java.io.IOException;
 
-public class MainServer {
+@SpringBootApplication
+public class ChatServerApplication {
     public static void main(String[] args) throws IOException, InterruptedException {
         ChatServer server = new ChatServer();
-        server.start(5555);
+        server.start(5566);
         server.blockUntilShutdown();
     }
 }
